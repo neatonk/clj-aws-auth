509a6b0f470070ccce8947dcd2fa86e8cffe0f2f
Kevin Neaton <neatonk@gmail.com>
2013-01-12 19:08:54 -0600

    Added codox for api documentation

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.18"]])
+                 [com.amazonaws/aws-java-sdk "1.3.18"]]
+  :plugins [[codox "0.6.4"]]
+  :codox {:output-dir "codox"
+          :src-dir-uri "http://github.com/neatonk/clj-aws-auth/blob/master"
+          :src-linenum-anchor-prefix "L"})
